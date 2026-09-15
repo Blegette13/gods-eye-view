45972eb9fec8e2b7109c8961fb7436b0c783f7e5
Blegette13 <legettebrandon13@gmail.com>
2026-09-15 16:30:58 -0500

    Wire BDP RRC provider into standalone server

--- server/standalone/vite.config.js
+++ server/standalone/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import { createBrowserViteConfig } from '../../build/vite.js';
 import { localProviderPlugins } from '../providers/local.js';
+import { bdpRrcProviderPlugin } from '../providers/bdp-rrc.js';
 
 const root = fileURLToPath(new URL('../../', import.meta.url));
 
@@ -12,7 +13,7 @@ export default defineConfig(({ mode }) => {
     if (process.env[key] === undefined) process.env[key] = value;
   }
   return createBrowserViteConfig({
-    plugins: localProviderPlugins(),
+    plugins: [...localProviderPlugins(), bdpRrcProviderPlugin()],
     googleApiKey: process.env.GOOGLE_MAPS_API_KEY,
     cesiumToken: process.env.CESIUM_ION_TOKEN,
     host: process.env.HOST,
